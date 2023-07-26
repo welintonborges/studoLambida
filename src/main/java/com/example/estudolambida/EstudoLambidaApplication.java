452c9b915e2e70ee153b3811cea083a3bb311e73
Welinton Borges <Welinton.b.borges@gmail.com>
2023-07-26 11:54:34 -0300

    novos estudos

---
+++ src/main/java/com/example/estudolambida/EstudoLambidaApplication.java
@@ -0,0 +1,32 @@
+package com.example.estudolambida;
+
+import com.example.estudolambida.product.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class EstudoLambidaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EstudoLambidaApplication.class, args);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+        System.out.println("aqui");
+    }
+
+}
---
+++ src/test/java/com/example/estudolambida/EstudoLambidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudolambida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoLambidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
